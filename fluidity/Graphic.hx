49afa225a43ddd5c8cd993f76d76ab2756b8c624
Aaron Santiago <aaronsantiago@gmail.com>
2015-11-04 14:31:33 -0800

    initial docs setup

--- fluidity/Graphic.hx
+++
@@ -1,9 +0,0 @@
-
-package fluidity;
-
-enum Graphic {
-
-  Image(filename:String);
-  Animation(filename:String,numFrames:Int,frameLength:Int,loop:Bool);
-  SpriteSheet(filename:String,width:Int,height:Int,frames:Array<Int>,frameLength:Int,loop:Bool);
-}
